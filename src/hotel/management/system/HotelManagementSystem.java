47905b1e1d6e2a3c52cfbbf274ebcebed8262861
ManeeshDev <manishprashangamage23@gmail.com>
2022-07-27 12:19:01 +0530

    update gitignore file and test print

--- src/hotel/management/system/HotelManagementSystem.java
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -15,6 +15,7 @@ public class HotelManagementSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("ManeeshDev - hotel-management-system");
     }
     
 }
